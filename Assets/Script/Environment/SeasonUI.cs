77fd00e4cbc91045f086f4e7e992df0b2571874e
ElectroSpeed <150343183+ElectroSpeed@users.noreply.github.com>
2025-12-12 10:27:50 +0100

    Revert "Merge remote-tracking branch 'origin/Feat-FixEnvironmentalPower' into Feat-UI-Creation"
    This reverts commit 0aae30f83dbc64104ab46c5b00058fca34dd56aa, reversing
    changes made to 044af10d3c1a83fa5dc25d370ceeec59e2463f5c.

--- Assets/Script/Environment/SeasonUI.cs
+++ Assets/Script/Environment/SeasonUI.cs
@@ -1,8 +1,13 @@
 using UnityEngine;
 using System.Collections;
-using System;
-using System.Diagnostics;
-using UnityEngine.XR;
+
+public enum Season
+{
+    Spring,
+    Summer,
+    Autumn,
+    Winter
+}
 
 public class SeasonUI : MonoBehaviour
 {
@@ -10,96 +15,51 @@ public class SeasonUI : MonoBehaviour
 
     [SerializeField] private GameObject _seasonWheelUI;
 
-    [SerializeField] SeasonManager _seasonManager;
-    
-
-    private void Start()
+    public void SetSeason()
     {
-        _currentSeason = _seasonManager.currentSeason;
-
-    }
-
-    private void OnEnable()
-    {
-        _seasonManager.OnSeasonChanged += HandleSeasonChanged;
-        _seasonManager.OnSeasonForcedChange += HandleSeasonForcedChange;
-    }
-
-    private void HandleSeasonChanged(Season season)
-    {       
-        _currentSeason = season;
-        float delay = _seasonManager.seasonDuration;
-        switch (_currentSeason)
-        {
-            case Season.Spring:  
-                RotateWheel(46, delay, false);
-                break;
-            case Season.Summer:
-                RotateWheel(136, delay, false);
-                break;
-            case Season.Autumn:
-                RotateWheel(226, delay, false);
-                break;
-            case Season.Winter:
-                RotateWheel(316, delay, false);
-                break;
-        }
-        
-    }
-
-    private void HandleSeasonForcedChange(Season season)
-    {
-        _currentSeason = season;
-        float delay = 0.5f;
         switch (_currentSeason)
         {
             case Season.Spring:
-                RotateWheel(0, delay, true);
+                
+                RotateWheel(0);
                 break;
             case Season.Summer:
-                RotateWheel(90, delay, true);
+                RotateWheel(90);
                 break;
             case Season.Autumn:
-                RotateWheel(180, delay, true);
+                RotateWheel(180);
                 break;
             case Season.Winter:
-                RotateWheel(270, delay, true);
-                //quand c'est fini
-                HandleSeasonChanged(_currentSeason);
+                RotateWheel(270);
                 break;
         }
-
     }
 
-    public void RotateWheel(float targetAngle, float duration, bool isEase)
-{
-    StopAllCoroutines(); // empêche deux animations de se superposer
-    StartCoroutine(RotateWheelSmooth(targetAngle, duration, isEase));
-}
-
-private IEnumerator RotateWheelSmooth(float targetAngle, float duration, bool isEase)
-{
-    float time = 0f;
-
-    Quaternion start = _seasonWheelUI.transform.rotation;
-    Quaternion end = Quaternion.AngleAxis(targetAngle, Vector3.forward);
-
-    while (time < duration)
+    public void RotateWheel(float targetAngle, float duration = 1f)
     {
-        time += Time.deltaTime;
-        float t = time / duration;
-
-        if (isEase)   t = 1f - Mathf.Pow(1f - t, 3);
-        // cubic ease-out
-      
-
-        _seasonWheelUI.transform.rotation = Quaternion.Lerp(start, end, t);
-
-        yield return null;
+        StartCoroutine(RotateWheelEaseOutCoroutine(targetAngle, duration));
     }
 
-    _seasonWheelUI.transform.rotation = end;
-}
+    private IEnumerator RotateWheelEaseOutCoroutine(float targetAngle, float duration)
+    {
+        float time = 0f;
 
+        Quaternion startRot = _seasonWheelUI.transform.rotation;
+        Quaternion endRot = Quaternion.Euler(0, 0, targetAngle);
+
+        while (time < duration)
+        {
+            time += Time.deltaTime;
+            float t = time / duration;
+            t = 1f - Mathf.Pow(1f - t, 3); // Ease out cubic
+    
+
+            _seasonWheelUI.transform.rotation = Quaternion.Lerp(startRot, endRot, t);
+
+            yield return null;
+        }
+
+        _seasonWheelUI.transform.rotation = endRot;
+    }
 
 }
