b8937bfdebff6be4cd5089882ebf9f76343f9574
SuperPomme2Ter <SuperPomme2Ter@gmail.com>
2025-12-16 16:26:52 +0100

    Menu UI done

---
+++ Assets/Script/UI-Canvas/MenuFade.cs
@@ -0,0 +1,13 @@
+using System;
+using UnityEngine;
+
+public class MenuFade : MonoBehaviour
+{
+    [SerializeField] AnimationCurve _fadeCurve;
+    
+
+    private void Update()
+    {
+        throw new NotImplementedException();
+    }
+}
